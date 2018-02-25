594b48be27352d33cede4030b29f70d60575b3cc
ivovi <ivomurdoch@gmail.com>
2018-02-25 11:10:00 +1000

    Added shaders directory

--- src/Battlewrench/GameLauncher.c
+++ src/Battlewrench/GameLauncher.c
@@ -154,7 +154,7 @@ int main(){
     glGenVertexArrays(1, &VAO);
 
     glBindVertexArray(VAO);
-
+\
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
